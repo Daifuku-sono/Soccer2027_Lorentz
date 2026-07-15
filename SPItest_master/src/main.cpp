5b492047914b9c46c62fb023ade3474a7e6a1ac8
Daifuku-sono <Sono.gentiluomo.1015@gmail.com>
2026-07-15 15:17:47 +0900

    絶望

--- SPItest_master/src/main.cpp
+++ SPItest_master/src/main.cpp
@@ -1,124 +1,113 @@
 #include <Arduino.h>
-#include <SerialPIO.h>
-#include <SPI.h>
+#include "hardware/pio.h"
+#include "hardware/gpio.h"
 
-constexpr uint32_t USB_BAUD = 115200;
-constexpr uint32_t PIO_BAUD = 115200;
-constexpr uint8_t CH_NUM = 3;
+static constexpr uint SCK_PIN  = 20;
+static constexpr uint MOSI_PIN = 21;
+static constexpr uint MISO_PIN = 18;
+static constexpr uint CS_PIN   = 19;
 
-// Master 側のピン
-constexpr uint8_t TX_PINS[CH_NUM] = {2, 10, 6};
-constexpr uint8_t RX_PINS[CH_NUM] = {3, 11, 7};
+PIO pio = pio0;
+uint sm;
+uint pio_offset;
 
-// ハードウェアSPI(SPI0)のピン
-constexpr uint8_t SPI_MISO_PIN = 18;
-constexpr uint8_t SPI_CS_PIN   = 19;
-constexpr uint8_t SPI_SCK_PIN  = 20;
-constexpr uint8_t SPI_MOSI_PIN = 21;
-
-SerialPIO pioUart0(TX_PINS[0], RX_PINS[0]);
-SerialPIO pioUart1(TX_PINS[1], RX_PINS[1]);
-SerialPIO pioUart2(TX_PINS[2], RX_PINS[2]);
-
-SerialPIO* const uarts[CH_NUM] = {
-  &pioUart0, &pioUart1, &pioUart2
+// 2命令の簡易PIO SPI風テスト
+static const uint16_t master_instructions[] = {
+    pio_encode_out(pio_pins, 1) | (0 << 12), // SCK low, MOSI 1bit出力
+    pio_encode_in(pio_pins, 1)  | (1 << 12)  // SCK high, MISO 1bit読取
 };
 
-struct LineBuffer {
-  char buf[96];
-  uint8_t len = 0;
+static const pio_program_t master_program = {
+    .instructions = master_instructions,
+    .length = 2,
+    .origin = -1
 };
-LineBuffer rxbuf[CH_NUM];
 
-void clearBuffer(LineBuffer &b) {
-  b.len = 0;
-  b.buf[0] = '\0';
+bool spi_master_init() {
+    sm = pio_claim_unused_sm(pio, true);
+
+    pio_offset = pio_add_program(pio, &master_program);
+
+    pio_sm_config c = pio_get_default_sm_config();
+    sm_config_set_wrap(&c, pio_offset, pio_offset + 1);
+
+    sm_config_set_sideset_pins(&c, SCK_PIN);
+    sm_config_set_sideset(&c, 1, false, false);
+    sm_config_set_out_pins(&c, MOSI_PIN, 1);
+    sm_config_set_in_pins(&c, MISO_PIN);
+
+    // MSB先頭で8bit送受信
+    sm_config_set_out_shift(&c, false, true, 8);
+    sm_config_set_in_shift(&c, false, true, 8);
+
+    sm_config_set_clkdiv(&c, 30.0f);
+
+    pio_gpio_init(pio, SCK_PIN);
+    pio_gpio_init(pio, MOSI_PIN);
+    pio_gpio_init(pio, MISO_PIN);
+
+    gpio_init(CS_PIN);
+    gpio_set_dir(CS_PIN, GPIO_OUT);
+    gpio_put(CS_PIN, 1);
+
+    pio_sm_init(pio, sm, pio_offset, &c);
+    pio_sm_clear_fifos(pio, sm);
+    pio_sm_restart(pio, sm);
+    pio_sm_set_enabled(pio, sm, true);
+
+    return true;
 }
 
-void handleLine(uint8_t ch, const char* s) {
-  Serial.print("[MASTER] ch");
-  Serial.print(ch);
-  Serial.print(" <- ");
-  Serial.println(s);
-}
+uint8_t spi_master_txrx(uint8_t tx) {
+    gpio_put(CS_PIN, 0);
+    delayMicroseconds(2);
 
-void pollPort(uint8_t ch) {
-  SerialPIO &p = *uarts[ch];
-  while (p.available() > 0) {
-    char c = (char)p.read();
-    if (c == '\r') continue;
-    if (c == '\n') {
-      rxbuf[ch].buf[rxbuf[ch].len] = '\0';
-      if (rxbuf[ch].len > 0) {
-        handleLine(ch, rxbuf[ch].buf);
-      }
-      clearBuffer(rxbuf[ch]);
-      continue;
+    pio_sm_put_blocking(pio, sm, (uint32_t)tx << 24);
+
+    uint32_t t0 = millis();
+    while (pio_sm_is_rx_fifo_empty(pio, sm)) {
+        if (millis() - t0 > 100) {
+            Serial.println("[Error] Rx Timeout");
+            gpio_put(CS_PIN, 1);
+            return 0xFF;
+        }
+        delay(1);
     }
-    if (rxbuf[ch].len < sizeof(rxbuf[ch].buf) - 1) {
-      rxbuf[ch].buf[rxbuf[ch].len++] = c;
-    } else {
-      clearBuffer(rxbuf[ch]); // 取りこぼし防止
-    }
-  }
-}
 
-void sendAll(uint32_t seq) {
-  for (uint8_t ch = 0; ch < CH_NUM; ch++) {
-    uarts[ch]->print("M,");
-    uarts[ch]->print(ch);
-    uarts[ch]->print(",");
-    uarts[ch]->println(seq);
-  }
+    uint8_t rx = (uint8_t)(pio_sm_get(pio, sm) & 0xFF);
+
+    delayMicroseconds(2);
+    gpio_put(CS_PIN, 1);
+    return rx;
 }
 
 void setup() {
-  Serial.begin(USB_BAUD);
-  delay(1500);
+    Serial.begin(115200);
+    delay(1500);
 
-  for (uint8_t i = 0; i < CH_NUM; i++) {
-    uarts[i]->begin(PIO_BAUD);
-    clearBuffer(rxbuf[i]);
-  }
+    Serial.println("BOOT");
 
-  // ハードウェアSPIの初期化
-  SPI.setRX(SPI_MISO_PIN);
-  SPI.setCS(SPI_CS_PIN);
-  SPI.setSCK(SPI_SCK_PIN);
-  SPI.setTX(SPI_MOSI_PIN);
-  SPI.begin();
-  
-  pinMode(SPI_CS_PIN, OUTPUT);
-  digitalWrite(SPI_CS_PIN, HIGH);
+    if (!spi_master_init()) {
+        Serial.println("[Error] PIO init failed");
+        while (true) delay(1000);
+    }
 
-  Serial.println("MASTER start");
+    Serial.println("--- Master Ready ---");
 }
 
 void loop() {
-  static uint32_t lastSend = 0;
-  static uint32_t seq = 0;
+    uint8_t tx_data = 0x12;
 
-  if (millis() - lastSend >= 500) {
-    lastSend = millis();
-    sendAll(seq);
+    Serial.print("Sending: 0x");
+    if (tx_data < 0x10) Serial.print('0');
+    Serial.print(tx_data, HEX);
+    Serial.print(" ... ");
 
-    // SPIでデータ送信
-    char spiTxBuf[32];
-    int len = snprintf(spiTxBuf, sizeof(spiTxBuf), "SPI,%lu", seq);
-    
-    digitalWrite(SPI_CS_PIN, LOW);
-    SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
-    SPI.transfer(spiTxBuf, len);
-    SPI.endTransaction();
-    digitalWrite(SPI_CS_PIN, HIGH);
+    uint8_t r = spi_master_txrx(tx_data);
 
-    Serial.print("[MASTER] SPI -> ");
-    Serial.println(spiTxBuf);
+    Serial.print("Received: 0x");
+    if (r < 0x10) Serial.print('0');
+    Serial.println(r, HEX);
 
-    seq++;
-  }
-
-  for (uint8_t ch = 0; ch < CH_NUM; ch++) {
-    pollPort(ch);
-  }
+    delay(1000);
 }
